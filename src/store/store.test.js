25fc0643db0b11fe637468eb1d405d54a6b4c979
Tomasz Wilczek <twilczek88@gmail.com>
2019-03-11 20:55:19 +0100

    add store unit tests

---
+++ src/store/store.test.js
@@ -0,0 +1,58 @@
+import store from './index';
+
+const EXPECTED = {
+  INPUT_VALUE: 'hello',
+  ID: 4,
+}
+
+const toDoArrayMock = [
+  {
+    id: 2,
+    label: 'test1',
+    complete: false,
+  },
+  {
+    id: 3,
+    label: 'test2',
+    complete: true,
+  },
+  {
+    id: 0,
+    label: 'test3',
+    complete: true,
+  },
+];
+
+const changeEventMock = { target: { value: EXPECTED.INPUT_VALUE } };
+
+it('calculates ID', () => {
+  expect(store.calculateId(toDoArrayMock)).toBe(EXPECTED.ID);
+});
+
+it('handles change event', () => {
+  store.handleInputChange(changeEventMock);
+  expect(store.inputValue).toBe(EXPECTED.INPUT_VALUE);
+});
+
+it('adds new element', () => {
+  expect(store.toDoArray.length).toBe(0);
+  store.addToDo();
+  expect(store.toDoArray[0].label).toBe(EXPECTED.INPUT_VALUE);
+  expect(store.toDoArray[0].id).toBe(0);
+  expect(store.toDoArray[0].complete).toBe(false);
+  expect(store.toDoArray.length).toBe(1);
+});
+
+it('toggles todo item', () => {
+  store.toggleComplete(0);
+  expect(store.toDoArray[0].complete).toBe(true);
+
+  store.toggleComplete(0);
+  expect(store.toDoArray[0].complete).toBe(false);
+});
+
+it('removes todo item', () => {
+  expect(store.toDoArray.length).toBe(1);
+  store.removeTodo(0);
+  expect(store.toDoArray.length).toBe(0);
+});
